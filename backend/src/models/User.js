555cebc8ca288d248daecec15b1fbafb9de9dcfd
Piyush_Tomar <piyushtomarp@gmail.com>
2026-02-13 00:48:12 +0530

    feat(backend): Add input trimming to User model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -5,15 +5,18 @@ const userSchema = mongoose.Schema({
     firstName: {
         type: String,
         required: true,
+        trim: true,
     },
     lastName: {
         type: String,
         required: true,
+        trim: true,
     },
     email: {
         type: String,
         required: true,
         unique: true,
+        trim: true,
     },
     password: {
         type: String,
