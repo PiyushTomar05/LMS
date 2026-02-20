829496ba60d11a92adaf46c801a87cad5ae0b5f0
Piyush_Tomar <piyushtomarp@gmail.com>
2026-02-20 23:51:07 +0530

    refactor(backend): move utility scripts to scripts directory and update paths

--- backend/scripts/seed.js
+++ backend/scripts/seed.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const User = require('./models/User');
-const University = require('./models/University');
-const connectDB = require('./config/db');
+const User = require('../src/models/User');
+const University = require('../src/models/University');
+const connectDB = require('../src/config/db');
 
 const seedData = async () => {
     await connectDB();
